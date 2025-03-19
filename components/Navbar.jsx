627ecc769922a33f3d6628953135a9e0e6139f02
faik.bagci@hotmail.de <faik.bagci@hotmail.de>
2025-03-20 00:19:42 +0100

    styling aktualisiert und angepasst

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,41 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/components/Navbar.scss';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/components/Navbar.scss";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div>
-      <h1>Bilal's Portfolio</h1>
+      <div className="allTogether">
+      <div className="navbar-brand">
+        <Link to="/" className="logo" onClick={() => setMenuOpen(false)}>
+          <i class="fa-brands fa-connectdevelop"></i>
+          <h1>
+            Bilal <br /> Bagci
+          </h1>
+        </Link>
+
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          <i
+            className={`fa ${menuOpen ? "fa-times" : "fa-bars"}`}
+            aria-hidden="true"></i>
+        </button>
       </div>
-      <div>
-      <div className="footer-section links">
-          <ul className="foot">
-            <li>
-              <a
-                href="https://github.com/webdevbfb"
-                target="_blank"
-                rel="noopener noreferrer">
-                <i className="fa-brands fa-github" aria-hidden="true"></i>{" "}
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/bilal-f-bagci-a4543a332/"
-                target="_blank"
-                rel="noopener noreferrer">
-                <i className="fa-brands fa-linkedin" aria-hidden="true"></i>{" "}
-              </a>
-            </li>
-          </ul>
+
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <Link to="/" onClick={() => setMenuOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" onClick={() => setMenuOpen(false)}>
+              Projekte
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={() => setMenuOpen(false)}>
+              Kontakt
+            </Link>
+          </li>
+        </ul>
+
+        <div className="social-links">
+          <a
+            href="https://github.com/webdevbfb"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bilal-f-bagci-a4543a332/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
         </div>
       </div>
-      <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/projects">Projekte</Link></li>
-        <li><Link to="/contact">Kontakt</Link></li>
-      </ul>
       </div>
     </nav>
   );
